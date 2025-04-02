6b2744ee726ac59a6632e754fc330ab172645c70
Joël Gaillard <joelgaillard@joelgaillard-4.local>
2025-04-02 13:56:19 +0200

    feat: add EventCard component

---
+++ components/Base/EventCard.vue
@@ -0,0 +1,49 @@
+<script setup>
+const router = useRouter();
+
+const props = defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  place: {
+    type: String,
+    required: true,
+  },
+  image_url: {
+    type: String,
+    required: true,
+  },
+});
+
+const goToEvent = () => {
+  router.push(`/events/${props.id}`);
+};
+
+</script>
+
+<template>
+  <div class="card bg-base-100 shadow-sm w-full">
+    <figure>
+      <img :src="image_url" :alt="title" />
+    </figure>
+    <div class="card-body">
+      <div class="flex flex-row gap-5">
+        <div> {{ date }}</div>
+        <div> {{ place }}</div>
+      </div>
+      <h2 class="text-4xl">{{ title }}</h2>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary" @click="goToEvent">Learn more</button>
+      </div>
+    </div>
+  </div>
+</template>
