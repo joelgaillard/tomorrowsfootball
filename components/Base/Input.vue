52dcd9ef32143a8c7b3b0afecd515f59d13145d8
Joël Gaillard <joelgaillard@joelgaillard-4.local>
2025-04-09 10:44:04 +0200

    fix(giveaway): adjust text size and spacing for better layout consistency

--- components/Base/Input.vue
+++ components/Base/Input.vue
@@ -1,6 +1,6 @@
 <template>
     <fieldset class="form-control flex flex-col gap-1 w-full">
-        <label v-if="label" class="label text text-3xl font-semibold text-secondary-content">
+        <label v-if="label" class="label text text-2xl font-semibold text-secondary-content">
             {{ label }}
         </label>
         <input class=" border-primary-content bg-base-200 focus:outline-none focus:border-secondary "
@@ -8,7 +8,7 @@
             @input="$emit('update:modelValue', $event.target.value)"
             :type="type"
             :placeholder="placeholder"
-            :class="['input input-bordered text-3xl py-7 px-6 w-full ', error ? 'input-error' : '']"
+            :class="['input input-bordered text-2xl py-7 px-6 w-full ', error ? 'input-error' : '']"
             :required="required"
         />
         <p v-if="error" class="text-error text-xl ml-2">{{ error }}</p>
