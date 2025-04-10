3d7f548a5d1ff2d66565fb981172e09c1ef56c19
LucaCDRocha <lucadrocha@gmail.com>
2025-04-10 11:07:08 +0200

    feat(player): add BasePlayer component with video support and integrate into serie page

---
+++ components/Base/Player.vue
@@ -0,0 +1,31 @@
+<script setup>
+	import "vidstack/bundle";
+
+	const props = defineProps({
+		title: {
+			type: String,
+			default: "Generique",
+		},
+		videoSrc: {
+			type: String,
+			default: "video/Generique.mp4",
+		},
+		posterSrc: {
+			type: String,
+			default: "img/serie/Generique.png",
+		},
+	});
+</script>
+
+<template>
+	<div class="flex rounded-2xl w-full lg:w-1/2 h-full overflow-hidden border border-secondary">
+		<media-player :title="title" :src="videoSrc">
+			<ClientOnly>
+				<media-provider
+					><media-poster class="vds-poster" :src="posterSrc" :alt="`Poster de l'episode ${title}`"></media-poster
+				></media-provider>
+				<media-video-layout small-when="never"></media-video-layout>
+			</ClientOnly>
+		</media-player>
+	</div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,38 +1,47 @@
+import { vite as vidstack } from "vidstack/plugins";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    head: {
-      title: "TomorroW's Football",
-      link: [
-        {
-          rel: "stylesheet",
-          href: "https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200",
-        },
-        {
-          rel: "stylesheet",
-          href: "https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&display=swap",
-        },
-      ],
-    },
-    baseURL: "/",
-  },
-  compatibilityDate: "2024-11-01",
-  devtools: { enabled: false },
-  eslint: {
-    config: {
-      stylistic: {
-        quotes: "single",
-      },
-    },
-  },
-  css: ["~/assets/app.css"],
-  modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/ui", "@nuxt/icon"],
-  runtimeConfig: {
-    MAILCHIMP_API_KEY: process.env.MAILCHIMP_API_KEY,
-    MAILCHIMP_AUDIENCE_ID: process.env.MAILCHIMP_AUDIENCE_ID,
-  },
-  ssr: true,
-  nitro: {
-    preset: "netlify",
-  },
+	app: {
+		head: {
+			title: "TomorroW's Football",
+			link: [
+				{
+					rel: "stylesheet",
+					href: "https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200",
+				},
+				{
+					rel: "stylesheet",
+					href: "https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&display=swap",
+				},
+			],
+		},
+		baseURL: "/",
+	},
+	vue: {
+		compilerOptions: {
+			isCustomElement: (tag) => tag.startsWith("media-"),
+		},
+	},
+	vite: {
+		plugins: [vidstack()],
+	},
+	compatibilityDate: "2024-11-01",
+	devtools: { enabled: false },
+	eslint: {
+		config: {
+			stylistic: {
+				quotes: "single",
+			},
+		},
+	},
+	css: ["~/assets/app.css"],
+	modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/ui", "@nuxt/icon"],
+	runtimeConfig: {
+		MAILCHIMP_API_KEY: process.env.MAILCHIMP_API_KEY,
+		MAILCHIMP_AUDIENCE_ID: process.env.MAILCHIMP_AUDIENCE_ID,
+	},
+	ssr: true,
+	nitro: {
+		preset: "netlify",
+	},
 });
